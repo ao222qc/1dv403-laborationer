a3e594f30405a712dc65b1a60ba403dbffc35806
ao222qc <ao222qc@student.lnu.se>
2014-11-24 14:43:13 +0000

    Still got problem with textmessages overlapping, but rest works

--- 2-labbymezzage/messageboard.js
+++ 2-labbymezzage/messageboard.js
@@ -12,81 +12,83 @@ var MessageBoard =
         
         var entersend = document.getElementById("textarea");
         entersend.onkeypress=function(e){if (e.keyCode == 13 && !e.shiftKey)
-                {
-                e.preventDefault();
-                MessageBoard.sendMessage();
-                }
+        {e.preventDefault(); MessageBoard.sendMessage();}
     };
        
     },
-    sendMessage:function()
-    {
-    var textInput = document.getElementById("textarea").value;
+        sendMessage:function()
+        {
+            var textInput = document.getElementById("textarea").value;
     
-    MessageBoard.messages.push(new Message(textInput, new Date()));
+            MessageBoard.messages.push(new Message(textInput, new Date()));
     
-    MessageBoard.amountOfMessages++;
+            MessageBoard.amountOfMessages++;
     
-    document.getElementById("numberOfMessages").innerHTML = "Antal Meddelanden: " + MessageBoard.amountOfMessages;
-    
-    
-    var input = MessageBoard.messages.length-1;
+            document.getElementById("numberOfMessages").innerHTML = "Antal Meddelanden: " + MessageBoard.amountOfMessages;
     
     
-    MessageBoard.renderMessage(input);
-    },
+            var input = MessageBoard.messages.length-1;
     
-    renderMessage:function(input)
-    {
+    
+            MessageBoard.renderMessage(input);
+    
+        },
+    
+        renderMessage:function(input)
+        {
         
-        var text = document.createElement("h7");
-        text.innerHTML = MessageBoard.messages[input].getHTMLText();
-        messagearea.appendChild(text);
+            var text = document.createElement("h7");
+            text.innerHTML = MessageBoard.messages[input].getHTMLText();
+            messagearea.appendChild(text);
     
-        var time = document.createElement("p");
-        time.innerHTML = MessageBoard.messages[input].getDateText();
-        messagearea.appendChild(time);
+            var time = document.createElement("p");
+            time.innerHTML = MessageBoard.messages[input].getDateText();
+            messagearea.appendChild(time);
         
-        var clock = document.createElement('img');
-        clock.src = 'CLOCK.jpg';
-        clock.id = 'clock';
-        messagearea.appendChild(clock);
+            var clock = document.createElement('img');
+            clock.src = 'CLOCK.jpg';
+            clock.id = 'clock';
+            messagearea.appendChild(clock);
         
-        var dustbin = document.createElement('img');
-        dustbin.src = 'dustbin.jpg';
-        dustbin.id = 'dustbin';
-        messagearea.appendChild(dustbin);
+            var dustbin = document.createElement('img');
+            dustbin.src = 'dustbin.jpg';
+            dustbin.id = 'dustbin';
+            messagearea.appendChild(dustbin);
         
-       dustbin.onclick = function(e)
-       {
-           if (confirm("Är du säker på att du vill radera meddelandet?"))
-           {
-           MessageBoard.numberOfMessages = MessageBoard.numberOfMessages -1;
-           text.parentNode.removeChild(text);
-           clock.parentNode.removeChild(clock);
-           dustbin.parentNode.removeChild(dustbin);
-           time.parentNode.removeChild(time);
-           MessageBoard.amountOfMessages -= 1;
-           document.getElementById("numberOfMessages").innerHTML = "Antal meddelanden: " + MessageBoard.amountOfMessages;
-           }
-       },
+        
+        
+            dustbin.onclick = function(e)
+            {
+                 if (confirm("Är du säker på att du vill radera meddelandet?"))
+                {
+                MessageBoard.numberOfMessages = MessageBoard.numberOfMessages -1;
+                text.parentNode.removeChild(text);
+                clock.parentNode.removeChild(clock);
+                dustbin.parentNode.removeChild(dustbin);
+                time.parentNode.removeChild(time);
+                MessageBoard.amountOfMessages -= 1;
+                document.getElementById("numberOfMessages").innerHTML = "Antal meddelanden: " + MessageBoard.amountOfMessages;
+                }
+            },
        
-       clock.onclick = function(e)
-       {
-           alert(MessageBoard.messages[input].getDate());
-       };
+            clock.onclick = function(e)
+            {
+                 alert(MessageBoard.messages[input].getDate());
+            };
        
-    },
-    
-    renderMessages:function()
+       
+        },
+        
+
+    /*renderMessages:function()
     {
         for (var i = 0; i > MessageBoard.messages.length; i++) 
         {
          MessageBoard.renderMessage(i);
         }
         
-    }
-  
+    },*/
+    
 };
 
 window.onload = MessageBoard.init;
